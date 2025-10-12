203f5b45de04812507ae3a2c05156e301e0a0be2
Dutkercz <dutkercz@gmail.com>
2025-10-12 03:02:35 -0300

    Adicionando o Sort.Direction na paginação de resultados em findAll (do PersonCotroller)

--- src/main/java/dutkercz/com/github/controllers/PersonController.java
+++ src/main/java/dutkercz/com/github/controllers/PersonController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -54,8 +55,11 @@ public class PersonController implements PersonControllerDocs {
                     APPLICATION_YAML_VALUE})
     @Override
     public ResponseEntity<Page<PersonDTO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
-                                            @RequestParam(value = "size", defaultValue = "12") Integer size) {
-        Pageable pageable = PageRequest.of(page, size);
+                                                   @RequestParam(value = "size", defaultValue = "12") Integer size,
+                                                   @RequestParam(value = "direction", defaultValue = "asc") String direction) {
+
+        Sort.Direction sortDirection = direction.equalsIgnoreCase("desc") ? Sort.Direction.DESC : Sort.Direction.ASC;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "firstName"));
         return ResponseEntity.ok(personService.findAll(pageable));
     }
 
--- src/main/java/dutkercz/com/github/services/PersonService.java
+++ src/main/java/dutkercz/com/github/services/PersonService.java
@@ -99,7 +99,7 @@ public class PersonService {
         dto.add(linkTo(methodOn(PersonController.class).findById(dto.getId())).withSelfRel().withType("GET"));
         dto.add(linkTo(methodOn(PersonController.class).delete(dto.getId())).withRel("delete").withType("DELETE"));
         dto.add(linkTo(methodOn(PersonController.class).disablePerson(dto.getId())).withRel("disable").withType("PATCH"));
-        dto.add(linkTo(methodOn(PersonController.class).findAll(1, 12)).withRel("findAll").withType("GET"));
+        dto.add(linkTo(methodOn(PersonController.class).findAll(1, 12, "asc")).withRel("findAll").withType("GET"));
         dto.add(linkTo(methodOn(PersonController.class).create(dto, null)).withRel("create").withType("POST"));
         dto.add(linkTo(methodOn(PersonController.class).update(dto)).withRel("update").withType("PUT"));
 
